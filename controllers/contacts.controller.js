ff06f0f82482fd184745a29f9330b767bc1c1bea
Julia-itcourse <juliaartemenko93@gmail.com>
2021-02-08 11:01:29 +0100

    adds validation

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -15,7 +15,6 @@ class ContactsController {
       .readFile(contactsPath, "utf-8")
       .then((data) => {
         const contacts = JSON.parse(data)
-        console.table(contacts)
         res.json(contacts)
       })
       .catch((error) => console.error(error))
@@ -29,12 +28,11 @@ class ContactsController {
         const result = JSON.parse(data).find(
           (contact) => contact.id === contactId
         )
+       
         if (result) {
-          console.log("We found:")
-          console.table(result)
-          res.json(result)
+          res.status(200).json(result)
         } else {
-          console.log(`Contact with ${contactId} ID doesn't excist!`)
+            res.status(404).send({message: "not found"})
         }
       })
       .catch((error) => console.error(error))
@@ -50,9 +48,8 @@ class ContactsController {
         )
         fsPromises.writeFile(contactsPath, JSON.stringify(updatedContacts))
 
-        console.log("Updated contacts:")
-        console.table(updatedContacts)
-        res.json(updatedContacts)
+        // res.json(updatedContacts)
+        res.status(200).send({ message: "Contact deleted" })
       })
       .catch((error) => console.error(error))
   }
@@ -69,131 +66,70 @@ class ContactsController {
 
         fsPromises.writeFile(contactsPath, JSON.stringify(updatedContacts))
 
-        console.log("Updated contacts:")
-        console.table(updatedContacts)
-        res.json(updatedContacts)
+        // res.json(updatedContacts)
+        res.status(201).send(newContact)
       })
       .catch((error) => console.error(error))
   }
 
-  updateContact(req, res){
-    const { name, email, phone } = req.body
-    const contactId = +req.params.contactId
+ updateContact(req, res) {
+    const { body } = req;
+    const { contactId } = req.params;
 
-    fsPromises
-    .readFile(contactsPath, "utf-8")
-    .then((data) => {
-        const contacts = JSON.parse(data);
-      const result = contacts.find(
-        (contact) => contact.id === contactId
-      )
-      if (result) {
-       result.name = name;
-       result.email = email;
-       result.phone = phone;
-       
+    fsPromises.readFile(contactsPath, 'utf-8')
+      .then(data => {
+        const newContacts = JSON.parse(data).map(contact => {
+         return contact = contact.id == contactId ? { ...contact, ...body } : contact; 
+          }
+        )
+        const contactToChange = newContacts.filter(contact => contact.id == contactId)
+  
+        fsPromises.writeFile(contactsPath, JSON.stringify(newContacts));
+  
+        if (!contactToChange.length) {
+          return res.status(404).send({ message: "Not found" })
+        }
+  
+       res.status(200).json(contactToChange);
+      })
+      .catch(err => console.log(err))
+  
+  }
 
-        fsPromises.writeFile(contactsPath, JSON.stringify(contacts));
-    res.json(result)
-      }
-      else {
-        console.log(`Contact with ${contactId} ID doesn't excist!`)
-      }
+//* validation
+
+
+  validateAddContact(req, res, next) {
+    const validationRules = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      phone: Joi.string().required(),
     })
-    .catch((error) => console.error(error))
 
+    const validationResult = validationRules.validate(req.body)
 
+    if (validationResult.error) {
+      return res.status(400).send({ message: "missing required name field" })
+    }
+
+    next()
+  }
+
+  validateUpdateContact(req, res, next) {
+    const validationRules = Joi.object({
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      phone: Joi.string().required(),
+    }).min(1)
+
+    const validationResult = validationRules.validate(req.body)
+
+    if (validationResult.error) {
+      return res.status(400).send({ message: "missing fields" })
+    }
+
+    next()
   }
 }
 
 module.exports = new ContactsController()
-
-//*from prev branch
-// const { v4: uuidv4 } = require("uuid")
-// const { promises: fsPromises } = require("fs")
-// const path = require("path")
-
-// const contactsPath = path.join(__dirname, "db/contacts.json")
-
-// function listContacts() {
-//   fsPromises
-//     .readFile(contactsPath, "utf-8")
-//     .then((data) => {
-
-//       const contacts = JSON.parse(data)
-//       console.table(contacts)
-//     })
-//     .catch((error) => console.error(error))
-// }
-
-// function getContactById(contactId) {
-//   fsPromises
-//     .readFile(contactsPath, "utf-8")
-//     .then((data) => {
-//       const result = JSON.parse(data).find(
-//         (contact) => contact.id === contactId
-//       )
-//       if (result) {
-//         console.log("We found:")
-//         console.table(result)
-//       } else {
-//         console.log(`Contact with ${contactId} ID doesn't excist!`)
-//       }
-//     })
-//     .catch((error) => console.error(error))
-// }
-
-// // function removeContact(contactId) {
-// //   fsPromises
-// //     .readFile(contactsPath, "utf-8")
-// //     .then((data) => {
-// //       const updatedContacts = JSON.parse(data).filter(
-// //         (contact) => contact.id !== contactId
-// //       )
-// //       fsPromises.writeFile(contactsPath, JSON.stringify(updatedContacts))
-// //     })
-// //     .then(result => {
-// //       console.log("Updated contacts:");
-// //       console.table(result)
-// //     })
-// //     .catch((error) => console.error(error))
-// // }
-
-// function removeContact(contactId) {
-//   fsPromises
-//     .readFile(contactsPath, "utf-8")
-//     .then((data) => {
-//       const updatedContacts = JSON.parse(data).filter(
-//         (contact) => contact.id !== contactId
-//       )
-//       fsPromises.writeFile(contactsPath, JSON.stringify(updatedContacts))
-
-//       console.log("Updated contacts:");
-//       console.table(updatedContacts)
-//     })
-//     .catch((error) => console.error(error))
-// }
-
-// function addContact(name, email, phone) {
-//   const newContact = { id: uuidv4(), name, email, phone }
-//   fsPromises
-//     .readFile(contactsPath, "utf-8")
-//     .then((data) => {
-//       const updatedContacts = JSON.parse(data)
-
-//       updatedContacts.push(newContact)
-
-//       fsPromises.writeFile(contactsPath, JSON.stringify(updatedContacts))
-
-//       console.log("Updated contacts:");
-//       console.table(updatedContacts)
-//     })
-//     .catch((error) => console.error(error))
-// }
-
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-// }
--- routes/contacts.routes.js
+++ routes/contacts.routes.js
@@ -6,23 +6,20 @@ const router = Router();
 router.get('/', ContactsController.listContacts);
 router.get(
   '/:contactId',
-//   ContactsController.validateContactId,
   ContactsController.getContactById,
 );
 router.post(
   '/',
-//   ContactsController.validateAddContact,
+  ContactsController.validateAddContact,
   ContactsController.addContact,
 );
 router.delete(
   '/:contactId',
-//   ContactsController.validateContactId,
   ContactsController.removeContact,
 );
 router.patch(
   '/:contactId',
-//   ContactsController.validateContactId,
-//   ContactsController.validateUpdateContact,
+  ContactsController.validateUpdateContact,
   ContactsController.updateContact,
 );
 
